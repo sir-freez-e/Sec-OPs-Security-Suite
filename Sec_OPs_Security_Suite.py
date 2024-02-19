72df1bcc0bdfef667b953fe333cf2d5976756b79
Mawuli Kudjawu <ykudjawu@gmail.com>
2024-02-19 10:24:41 -0600

    Update Sec_OPs_Security_Suite.py
    base code added

--- Sec_OPs_Security_Suite.py
+++ Sec_OPs_Security_Suite.py
@@ -1,1 +1,26 @@
 
+import cv2
+import numpy as np
+import tensorflow as tf
+import customtkinter
+import pystray
+from PIL import Image
+from tkinter import *
+
+class App(customtkinter.CTk):
+   def __init__(self):
+       super().__init__()    
+
+app = App()
+root = app
+
+
+
+
+
+
+# Window settings
+app.title('Sec-OPs Suite')
+app.geometry("400x300+10+10")
+app.mainloop()
+
